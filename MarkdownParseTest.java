205850b4e5ff674a5b0b4185f1e0b494ae7de8af
Anthony Truong <47471874+atruong39@users.noreply.github.com>
2022-01-28 12:57:31 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,27 @@
 import static org.junit.Assert.*;
+
+// import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.*;
+
+import java.util.List;
+
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks1() {
+        List<String> expected = List.of("https://something.com", "some-page.html");
+        try {
+            assertEquals(expected, MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
