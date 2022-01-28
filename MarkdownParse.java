e3b823b14214f41ac50a6b1caf0bcf4716984a49
kneeko97 <ncalsada@ucsd.edu>
2022-01-28 15:05:12 -0800

    currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 currentIndex: 0 nikocalsada@Nikos-Air markdown-parse %
    The symptom of a bug in test-file-4 is similar to test-file-2. The while loop enters an infinite loop as it continues searching for the end of the syntax []().

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -23,6 +23,7 @@ public class MarkdownParse {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
+            System.out.println("currentIndex: " + currentIndex);
         }
         return toReturn;
     }
